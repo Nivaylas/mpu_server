fbf1eeb30733c8d83df2109159f85b1349ac34fb
Salyavin <salyavin_github@outlook.com>
2019-08-02 17:39:08 +0800

    Update server.ino

--- server.ino
+++ server.ino
@@ -1,108 +1,56 @@
-#include <Wire.h>
+#include <wire.h>
 const int mpu_address = 0x68;
-int16_t ACC_X, ACC_Y;
-int firstX_read, secondX_read, thirdX_read;
-int firstY_read, secondY_read, thirdY_read;
-int result12_X, result12_Y;
-int result13_X, result13_Y;
-int result23_X, result23_Y;
-int counter12_X, counter13_X, counter23_X;
-int counter12_Y, counter13_Y, counter23_Y;
-int heap_X, heap_Y;
+const int detect_times = 173;
+int16_t rex, rey;
+int xyarr[detect_times][2];
 
-void setup() {
-  pinMode(11,OUTPUT);
-  Wire.begin();
-  Wire.beginTransmission(mpu_address);
-  Wire.write(0x6B);
-  Wire.write(0);
-  Wire.endTransmission(true);
-  Serial.begin(9600);
+void setup(){
+    pinMode(11,1);
+    Wire.begin();
+    Wire.beginTransmission(mpu_address);
+    Wire.write(0x6B);
+    Wire.write(0);
+    Wire.endTransmission(true);
+    Serial.begin(9600);
 }
 
-void loop() {
-  Wire.beginTransmission(mpu_address);
-  Wire.write(0x3B);
-  Wire.endTransmission(false);
-  Wire.requestFrom(mpu_address,4,true);
-  ACC_X = Wire.read() << 8 | Wire.read();
-  ACC_Y = Wire.read() << 8 | Wire.read();
-  firstX_read = ACC_X;
-  firstY_read = ACC_Y;
-  delay(500);
-  ACC_X = Wire.read() << 8 | Wire.read();
-  ACC_Y = Wire.read() << 8 | Wire.read();
-  secondX_read = ACC_X;
-  secondY_read = ACC_Y;
-  delay(500);
-  ACC_X = Wire.read() << 8 | Wire.read();
-  ACC_Y = Wire.read() << 8 | Wire.read();
-  thirdX_read = ACC_X;
-  thirdY_read = ACC_Y;
-  result12_X = abs(firstX_read - secondX_read);
-  result13_X = abs(firstX_read - thirdX_read);
-  result23_X = abs(secondX_read - thirdX_read);
-  result12_Y = abs(firstY_read - secondY_read);
-  result13_Y = abs(firstY_read - thirdY_read);
-  result23_Y = abs(secondY_read - thirdY_read);
-  if (result12_X >= 4300)
-  {
-    counter12_X = 1;
-  }
-  else
-  {
-    counter12_X = 0;
-  }
-  if (result12_Y >= 4300)
-  {
-    counter12_Y = 1;
-  }
-  else
-  {
-    counter12_Y = 0;
-  }
-  if (result13_X >= 4300)
-  {
-    counter13_X = 1;
-  }
-  else
-  {
-    counter13_X = 0;
-  }
-  if (result13_Y >= 4300)
-  {
-    counter13_Y = 1;
-  }
-  else
-  {
-    counter13_Y = 0;
-  }
-  if (result23_X >= 4300)
-  {
-    counter23_X = 1;
-  }
-  else
-  {
-    counter23_X = 0;
-  }
-  if (result23_Y >= 4300)
-  {
-    counter23_Y = 1;
-  }
-  else
-  {
-    counter23_Y = 0;
-  }
-  heap_X = counter12_X + counter13_X + counter23_X;
-  heap_Y = counter12_Y + counter13_Y + counter23_Y;
-  if (heap_X >= 2 or heap_Y >= 2)
-  {
-    digitalWrite(11,HIGH);
-    Serial.print(1);
-  }
-  else 
-  {
-    digitalWrite(11,LOW);
-    Serial.print(0);
-  }
+void loop(){
+    Wire.beginTransmission(mpu_address);
+    Wire.write(0x3B);
+    Wire.endTransmission(false);
+    Wire.requestFrom(mpu_address,4,true);
+    for (int i;i<detect_times-1;i++){
+        rex = Wire.read() << 8 | Wire.read();
+        rey = Wire.read() << 8 | Wire.read();
+            xyarr[i][0] = rex;
+            xyarr[i][1] = rey;
+            delay(3.5);
+    }
+    if (detect(xyarr)){
+            digitalWrite(11,1);
+            Serial.print(1);
+    }
+    else{
+            digitalWrite(11,0);
+            Serial.print(0);
+    }
 }
+
+float sigmoid(int x){
+    return (1 / (1 + exp(-x)));
+}
+
+bool detect(int arr[][2]){
+        int sumx, sumy;
+        for (int j;j<detect_times-2;j++){
+            diffx = sigmoid(abs(arr[j][0] - arr[j+1][0]));
+            diffy = sigmoid(abs(arr[j][1] - arr[j+1][1]));
+            sumx += diffx;
+            sumy += diffy;
+        }
+        if (sumx==172 or sumy==172)
+                return true;
+        else
+                return false;
+}
+
