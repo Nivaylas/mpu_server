88e9e87ac2af10459c3ae7e141ee3c6d2e55ab27
Salyavin <salyavin_github@outlook.com>
2020-01-18 01:42:04 +0800

    rewrite

--- mpu_server.ino
+++ mpu_server.ino
@@ -1,109 +1,25 @@
-#include <Wire.h>
-const int mpu_address = 0x68;
-int16_t ACC_X, ACC_Y;
-int firstX_read, secondX_read, thirdX_read;
-int firstY_read, secondY_read, thirdY_read;
-int result12_X, result12_Y;
-int result13_X, result13_Y;
-int result23_X, result23_Y;
-int counter12_X, counter13_X, counter23_X;
-int counter12_Y, counter13_Y, counter23_Y;
-int heap_X, heap_Y;
-
+#include "I2Cdev.h"                                                                              
+#include "MPU6050.h"
+#include "Wire.h"
+ 
+MPU6050 detect;
+int16_t ax, ay, az, gx, gy, gz;
+int16_t rec_ax, rec_ay, rec_az, rec_gx, rec_gy, rec_gz;
+ 
 void setup() {
-  pinMode(11,OUTPUT);
   Wire.begin();
-  Wire.beginTransmission(mpu_address);
-  Wire.write(0x6B);
-  Wire.write(0);
-  Wire.endTransmission(true);
-  Serial.begin(9600);
+  detect.initialize();
+  pinMode(13, OUTPUT);
 }
-
+ 
 void loop() {
-  Wire.beginTransmission(mpu_address);
-  Wire.write(0x3B);
-  Wire.endTransmission(false);
-  Wire.requestFrom(mpu_address,4,true);
-  ACC_X = Wire.read() << 8 | Wire.read();
-  ACC_Y = Wire.read() << 8 | Wire.read();
-  firstX_read = ACC_X;
-  firstY_read = ACC_Y;
-  delay(500);
-  ACC_X = Wire.read() << 8 | Wire.read();
-  ACC_Y = Wire.read() << 8 | Wire.read();
-  secondX_read = ACC_X;
-  secondY_read = ACC_Y;
-  delay(500);
-  ACC_X = Wire.read() << 8 | Wire.read();
-  ACC_Y = Wire.read() << 8 | Wire.read();
-  thirdX_read = ACC_X;
-  thirdY_read = ACC_Y;
-  result12_X = abs(firstX_read - secondX_read);
-  result13_X = abs(firstX_read - thirdX_read);
-  result23_X = abs(secondX_read - thirdX_read);
-  result12_Y = abs(firstY_read - secondY_read);
-  result13_Y = abs(firstY_read - thirdY_read);
-  result23_Y = abs(secondY_read - thirdY_read);
-  if (result12_X >= 4300)
-  {
-    counter12_X = 1;
+  detect.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+  delay(15);
+  detect.getMotion6(&rec_ax, &rec_ay, &rec_az, &rec_gx, &rec_gy, &rec_gz);
+  if (abs(rec_ax-ax)>7000 or abs(rec_ay-ay)>7000){
+    digitalWrite(13, HIGH);
   }
-  else
-  {
-    counter12_X = 0;
-  }
-  if (result12_Y >= 4300)
-  {
-    counter12_Y = 1;
-  }
-  else
-  {
-    counter12_Y = 0;
-  }
-  if (result13_X >= 4300)
-  {
-    counter13_X = 1;
-  }
-  else
-  {
-    counter13_X = 0;
-  }
-  if (result13_Y >= 4300)
-  {
-    counter13_Y = 1;
-  }
-  else
-  {
-    counter13_Y = 0;
-  }
-  if (result23_X >= 4300)
-  {
-    counter23_X = 1;
-  }
-  else
-  {
-    counter23_X = 0;
-  }
-  if (result23_Y >= 4300)
-  {
-    counter23_Y = 1;
-  }
-  else
-  {
-    counter23_Y = 0;
-  }
-  heap_X = counter12_X + counter13_X + counter23_X;
-  heap_Y = counter12_Y + counter13_Y + counter23_Y;
-  if (heap_X >= 2 or heap_Y >= 2)
-  {
-    digitalWrite(11,HIGH);
-    Serial.print(1);
-  }
-  else
-  {
-    digitalWrite(11,LOW);
-	Serial.print(0);
+  else{
+    digitalWrite(13, LOW);
   }
 }
-
